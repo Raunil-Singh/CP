ce7147b7386ffe06d04c4af84887f99a88e7a7ad
Raunil-Singh <raunilsingh.231cs148@nitk.edu.in>
2026-04-26 15:40:15 +0530

    Bottled  in the contest

---
+++ B_Artistic_Balance_Tree.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <queue>
+using namespace __gnu_pbds;
+using namespace std;
+
+#ifdef BG // use flag -DBG when compiling to enable
+#include <dbg.h>
+#define FAST_IO ;
+#else
+#define dbg(x, ...) ;
+#endif
+
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// order_of_key (k) : Number of items strictly smaller than k .
+// find_by_order(k) : K-th element in a set (counting from zero).
+// use less_equal for multiple entries
+
+#define int int64_t // upto 9.2 * (10^18)
+#define uint uint64_t // upto 1.8 * (10^19)
+#define inp(v) for(auto &temporary_variable : v) cin >> temporary_variable
+#define all(x) x.begin(), x.end()
+#define mpr(x, y) make_pair(x, y)
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+const long double eps = 1e-12;
+#define MOD ((int64_t) 998244353)
+
+inline void preprocessing() {
+
+    // Code here
+
+}
+
+
+priority_queue<int> pq[2];
+
+void solve(int tc_no) {
+    
+    // cout << "Case #" << tc_no << ": ";
+    int n, m;
+    cin >> n >> m;
+
+    for(int i = 0 ; i<n ; i++) {
+        int x; cin >> x;
+        pq[i&1].push(x);
+    }
+
+    bool flag[2] = {false, false};
+    for(int i = 0 ; i<m ; i++) {
+        int x; cin >> x; x--;
+        if(!flag[x&1]) {
+            pq[x&1].pop();
+            flag[x&1] = true;
+        } else if(!pq[x&1].empty() && pq[x&1].top() >= 0) pq[x&1].pop();
+    }
+
+    // if(!flag[0] && !pq[0].empty()) pq[0].pop();
+    // if(!flag[1] && !pq[1].empty()) pq[1].pop();
+
+    int ans = 0;
+    while(!pq[0].empty()) {
+        ans += pq[0].top();
+        pq[0].pop();
+    }
+    while(!pq[1].empty()) {
+        ans += pq[1].top();
+        pq[1].pop();
+    }
+
+    cout << ans << "\n";
+
+}
+// Note: Ensure all template parameters are updated!
+
+/****              Algorithm keypoints:
+    here:
+    
+    
+    
+****/
+
+int32_t main() {
+    #ifdef BG
+    auto begin = std::chrono::high_resolution_clock::now();
+    #else
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #endif
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    // cout << fixed;
+    cout << setprecision(10);
+
+    preprocessing();
+
+    int t{1};
+    cin >> t; // Update me!
+    for(int i{1} ; i<=t ; i++)
+        solve(i);
+
+    #ifdef BG
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n"; 
+    #endif
+
+    return 0;
+}
+
+
+// ======= CHECKLIST =======
+// Notebook  : 
+// Using it? : 
+// Sol Proof : 
+// algo noted: 
+// Update the number of testcases: 
+// Start coding ;)
---
+++ D_Permutation_Construction.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#ifdef BG // use flag -DBG when compiling to enable
+#include <dbg.h>
+#define FAST_IO ;
+#else
+#define dbg(x, ...) ;
+#endif
+
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// order_of_key (k) : Number of items strictly smaller than k .
+// find_by_order(k) : K-th element in a set (counting from zero).
+// use less_equal for multiple entries
+
+#define int int64_t // upto 9.2 * (10^18)
+#define uint uint64_t // upto 1.8 * (10^19)
+#define inp(v) for(auto &temporary_variable : v) cin >> temporary_variable
+#define all(x) x.begin(), x.end()
+#define mpr(x, y) make_pair(x, y)
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+const long double eps = 1e-12;
+#define MOD ((int64_t) 998244353)
+
+inline void preprocessing() {
+
+    // Code here
+
+}
+
+void solve(int tc_no) {
+    
+    // cout << "Case #" << tc_no << ": ";
+    int n;
+    cin >> n;
+    vector<int> v(n); inp(v);
+
+    vector<int> sum(n);
+    for(int i = 1 ; i<n ; i++) sum[i] = sum[i-1] + v[i-1];
+
+    ordered_set<int> elements(all(sum));
+    ordered_set<int> perm;
+    for(int i = 1 ; i<=n ; i++) perm.insert(i);
+
+    vector<int> res(n);
+    for(int i = n-1 ; i>=0 ; i--) {
+        int lesser = elements.order_of_key(sum[i]);
+        int tar = i - lesser;
+        auto x = perm.find_by_order(tar);
+        res[i] = *x;
+        perm.erase(x);
+        elements.erase(elements.find_by_order(lesser));
+    }
+
+    for(int num : res) cout << num << " ";
+    cout << "\n";
+
+}
+// Note: Ensure all template parameters are updated!
+
+/****              Algorithm keypoints:
+    here:
+    
+    
+    
+****/
+
+int32_t main() {
+    #ifdef BG
+    auto begin = std::chrono::high_resolution_clock::now();
+    #else
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #endif
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    // cout << fixed;
+    cout << setprecision(10);
+
+    preprocessing();
+
+    int t{1};
+    cin >> t; // Update me!
+    for(int i{1} ; i<=t ; i++)
+        solve(i);
+
+    #ifdef BG
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n"; 
+    #endif
+
+    return 0;
+}
+
+
+// ======= CHECKLIST =======
+// Notebook  : 
+// Using it? : 
+// Sol Proof : 
+// algo noted: 
+// Update the number of testcases: 
+// Start coding ;)
